53df4035562245d4bb9caff698e51e283d9423fa
vilkov <alexander.vilkov@gridnine.com>
2022-11-03 14:38:19 +0300

    security

--- src/main/kotlin/controller/AuthController.kt
+++ src/main/kotlin/controller/AuthController.kt
@@ -7,13 +7,14 @@ import org.springframework.http.ResponseEntity
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import security.jwt.JwtTokenProvider
 
 @RestController
-@RequestMapping("/api/v1/auth/")
+@RequestMapping("/api/v1/auth")
 class AuthController(
     @Autowired
     val authManager: AuthenticationManager,
@@ -23,6 +24,7 @@ class AuthController(
     val userService: UserServiceInterface
 ) {
 
+    @PostMapping("/login")
     fun login(@RequestBody authRequest: AuthRequest): ResponseEntity<MutableMap<String, String>> {
         try {
             val login = authRequest.login

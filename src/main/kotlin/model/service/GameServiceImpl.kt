f423cdbc7f62eb74244c4ef39e233ad60826724a
vilkov <alexander.vilkov@gridnine.com>
2022-11-03 09:47:08 +0300

    commit

--- src/main/kotlin/model/service/GameServiceImpl.kt
+++ src/main/kotlin/model/service/GameServiceImpl.kt
@@ -1,20 +1,48 @@
 package model.service
 
+import controller.GameServiceInterface
+import controller.WordServiceInterface
+import logger.Logger
 import model.Entity.Game
+import model.Entity.GameStatus
+import model.Entity.Message
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
-import org.springframework.stereotype.Service
 
-@Service
-class GameServiceImpl:GameServiceInterface {
-    override fun createGame(game: Game): Boolean {
+@Component
+class GameServiceImpl(
+    @Autowired
+    private val dbConnector: DataBaseConnector,
+    private val wordService: WordServiceInterface
+) : GameServiceInterface {
+    val logger = Logger("GameServiceImpl")
+
+    override fun createGame(game: Game, msgs: MutableList<Message>): Game? {
+        val word = wordService.findRandomWord(game.countLettersInHiddenWord, msgs)
+        game.hiddenWord = word?.wordValue.toString()
+        if (game.hiddenWord.isEmpty()) {
+            val messageTest = "Ошибка получения случайного слова длинной ${game.countLettersInHiddenWord}"
+            msgs.add(
+                Message(messageTest, "")
+            )
+            logger.debug(messageTest)
+            game.status = GameStatus.ERROR
+        }
+        return if (dbConnector.save(game)) {
+            game
+        } else {
+            val text = "Не удалось создать игру для полльзователя  ${game.userUid}"
+            msgs.add(Message(text, ""))
+            logger.debug(text)
+            null
+        }
+    }
+
+    override fun readUserGames(userUid: String, msgs: MutableList<Message>): List<Game> {
         TODO("Not yet implemented")
     }
 
-    override fun readUserGames(userUid: String): List<Game> {
-        TODO("Not yet implemented")
-    }
-
-    override fun updateGames(gameUid: String): Boolean {
+    override fun updateGames(gameUid: String, msgs: MutableList<Message>): Boolean {
         TODO("Not yet implemented")
     }
 }

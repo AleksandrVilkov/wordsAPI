3ddd33a253b6ae1f70e0ab35e84678fde4136b12
Vilkov Aleksandr <vilkovaleksandr@gmail.com>
2022-11-07 21:35:41 +0300

    commit

--- src/main/kotlin/WordsApplication.kt
+++ src/main/kotlin/WordsApplication.kt
@@ -1,9 +1,9 @@
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = ["src/main/kotlin"])
 class WordsApplication
 
 fun main(args: Array<String>) {
-	runApplication<WordsApplication>(*args)
+    runApplication<WordsApplication>(*args)
 }
